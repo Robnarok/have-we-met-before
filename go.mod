776ca96335606e7949c1c07a72c89a8716b7a3e6
Robnarok <robin-kraft@live.de>
2022-06-19 16:55:10 +0200

    add first Golio queries

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module have-we-met-before
 
 go 1.18
+
+require (
+	github.com/KnutZuidema/golio v0.0.0-20220228083517-e9a630a5acc7
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/KnutZuidema/golio"
+	"github.com/KnutZuidema/golio/api"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	foo := os.Getenv("APIKEY")
-	fmt.Printf("foo: %v\n", foo)
+	APIKEY := os.Getenv("APIKEY")
+	client := golio.NewClient(APIKEY,
+		golio.WithRegion(api.RegionEuropeWest),
+		golio.WithLogger(logrus.New().WithField("foo", "bar")))
+
+	summoner, _ := client.Riot.Summoner.GetByName("DreiAugenFlappe")
+	fmt.Printf("%s is a level %d summoner\n", summoner.Name, summoner.SummonerLevel)
 }
